5866bbbf9c07205d7fec409e0c951d08c5db374b
Radhika10-1997 <80508261+Radhika10-1997@users.noreply.github.com>
2021-06-07 10:56:28 +0530

    Work done

--- helloworld.java
+++ helloworld.java
@@ -4,6 +4,11 @@ public static void main(String args[])
 int a=3;
 int b=9;
 int c=a+b;
+  int d=a*b;
+  int e=a-b;
+  
 System.out.println(c);
+  System.out.println(d);
+  System.out.println(e);
 }
 }
